3880cb3a76291394aedc52b8fad92cc38360045f
annamar90 <78385939+annamar90@users.noreply.github.com>
2021-05-10 16:44:35 +0200

    Search engine and css elements
    App CSS: footer and signature
    App JS: Dictionary component
    Dictionary JS: search engine
    Dictionary CSS: no modifications

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 
-import logo from './SheCodes-logo.png';
-import './App.css';
+import logo from "./SheCodes-logo.png";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <img src={logo} alt="logo" className="img-fluid"/>
+      <img src={logo} alt="logo" className="img-fluid"/>
       </header>
+
+      <main>
+        <Dictionary />
+      </main>
+
+      <footer className="App-footer">
+        <small>
+        Coded by Anna Marasso
+        </small>
+      </footer>
+      </div>
     </div>
   );
 }
