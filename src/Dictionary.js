baaf7de6ec8a836650be401488235b5ed6df5a4a
annamar90 <78385939+annamar90@users.noreply.github.com>
2021-05-14 13:34:34 +0200

    Rendered components
    results, meaning components created
    definition, part of speech, example given

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults]  = useState(null);
 
     function handleResponse(response){
         console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function search(event) {
@@ -27,7 +30,7 @@ export default function Dictionary() {
     return <div className="Dictionary">  
                 <form onSubmit={search}>
                     <input type="search" onChange={handleKeywordChange} />
-
+                    <Results results={results}/>
                 </form>
             </div>;       
 }
